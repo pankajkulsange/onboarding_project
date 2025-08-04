ec60417442996c6102d0f03688ec392a541d8e8b
Pankaj Kulsange <85685465+pankajkulsange@users.noreply.github.com>
2025-08-04 16:32:04 +0530

    removed initial errors

--- new-joiner-qa-portal/src/components/Navbar.tsx
+++ new-joiner-qa-portal/src/components/Navbar.tsx
@@ -6,13 +6,13 @@ const Navbar = () => {
             <div className="container mx-auto">
                 <ul className="flex space-x-4">
                     <li>
-                        <Link href="/manager">
-                            <a className="text-white hover:text-gray-300">Manager</a>
+                        <Link href="/manager" className="text-white hover:text-gray-300">
+                            Manager
                         </Link>
                     </li>
                     <li>
-                        <Link href="/new-joiner">
-                            <a className="text-white hover:text-gray-300">New Joiner</a>
+                        <Link href="/new-joiner" className="text-white hover:text-gray-300">
+                            New Joiner
                         </Link>
                     </li>
                 </ul>
